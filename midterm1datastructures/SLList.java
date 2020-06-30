1bcfb595f2f3daed72791dbf78c3e394f6df5942
Ryan A. Nader <berkeleybear22ryan@berkeley.edu>
2020-06-30 16:01:11 -0700

    Updated changes.

--- midterm1datastructures/SLList.java
+++ midterm1datastructures/SLList.java
@@ -1,6 +1,23 @@
-public class SLList extends Object {
+public class SLList<T1> extends Object {
 
-    public static class Node {
-        
+    public static class Node<T2> {
+        public T2 item;
+        public Node next;
+
+        public Node(T2 item, Node<T2> next) {
+            this.item = item;
+            this.next = next;
+        }
     }
+
+
+
+//    public static void main(String[] args) {
+//        Node<String> tt = new Node<>("the", null);
+//        tt.next = new Node<String>("dog", null);
+//    }
+
+
+
+
 }
