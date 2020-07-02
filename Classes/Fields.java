521cbbd207c81d4c8a115abbe6f7a48cee754e9a
Monishwaran <monishwaran@macbook.local.com>
2020-07-02 16:02:36 -0700

    ryan checkout

--- Classes/Fields.java
+++ Classes/Fields.java
@@ -3,8 +3,10 @@ public class Fields{
 	public static class A{
 
 	/** Member variables */
-	public static String x = "A class, static field";
-	public String y = "Instance field y in A";
+	// public static String x = "A class, static field";
+	// public String y = "Instance field y in A";
+	public static int x;
+	public int y;
 
 	}
 
@@ -12,8 +14,10 @@ public class Fields{
 	public static class B extends A{
 
 	/** Member variables */
-	public static String x = "B class, static field";
-	public String y = "Instance field y in B";
+	// public static String x = "B class, static field";
+	// public String y = "Instance field y in B";
+	public static int x = 10;
+	public int y = 20;
 
 	}
 
@@ -21,7 +25,7 @@ public class Fields{
 		A a = new A();
 		B b = new B();
 		A ab = new B();
-		System.out.println(a.y);
+		// System.out.println(a.y);
 		System.out.println(b.y);
 		System.out.println(ab.y);
 	}
