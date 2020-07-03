026d394ca4cf1d3fef59387e72ec78584f6cdc9f
Ryan A. Nader <berkeleybear22ryan@berkeley.edu>
2020-07-03 01:16:17 -0700

    finished file

--- Midterm1Revision/MethodLookupStatic.java
+++ Midterm1Revision/MethodLookupStatic.java
@@ -35,14 +35,6 @@ public class MethodLookupStatic {
             System.out.println("I am in P of class B. (2 input)");
         }
 
-        public static void PA(int x){
-            System.out.println("I am method PA from class B. (1 input)");
-        }
-
-        public static void PA(int x, int y){
-            System.out.println("I am method PA from class B. (2 input)");
-        }
-
     }
 
     public static void main(String[] args) {
@@ -87,6 +79,47 @@ public class MethodLookupStatic {
          * then it does not ever look at the dynamic type. (Because static methods only care about static type.)
          */
         bb.P(1, 2); //I am in P of class B. (2 input)
+
+        /**
+         * aa: static type is A and the dynamic type is A --> This will look ONLY at the static
+         * type of aa or a super class and if the method is present it will pick that one
+         * otherwise raise a compile time error.
+         */
+        aa.PA(1); // I am method PA from class A. (1 input)
+        /**
+         * aa: static type is A and the dynamic type is A --> This will look ONLY at the static
+         * type of aa or a super class and if the method is present it will pick that one
+         * otherwise raise a compile time error.
+         */
+        aa.PA(1,2); // I am method PA from class A. (2 input)
+        /**
+         * ab: static type is A and the dynamic type is B --> This will look ONLY at the static
+         * type of aa or a super class and if the method is present it will pick that one
+         * otherwise raise a compile time error.
+         */
+        ab.PA(1); // I am method PA from class A. (1 input)
+        /**
+         * ab: static type is A and the dynamic type is B --> This will look ONLY at the static
+         * type of aa or a super class and if the method is present it will pick that one
+         * otherwise raise a compile time error.
+         */
+        ab.PA(1,2); // I am method PA from class A. (2 input)
+        /**
+         * bb: static type is B and the dynamic type is B --> This will look ONLY at the static
+         * type of aa or a super class and if the method is present it will pick that one
+         * otherwise raise a compile time error.
+         * Here there is no PA in B but it extends A so it will "look up" into A to find
+         * if there is a mewthod that exists
+         */
+        bb.PA(1); // I am method PA from class A. (1 input)
+        /**
+         * bb: static type is B and the dynamic type is B --> This will look ONLY at the static
+         * type of aa or a super class and if the method is present it will pick that one
+         * otherwise raise a compile time error.
+         * Here there is no PA in B but it extends A so it will "look up" into A to find
+         * if there is a mewthod that exists
+         */
+        bb.PA(1,2); // I am method PA from class A. (2 input)
 
 
 
