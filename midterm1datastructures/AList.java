c124b5d0511574f6030feaa973c8abf8dee28569
Ryan A. Nader <berkeleybear22ryan@berkeley.edu>
2020-07-04 22:38:32 -0700

    monish question lookup in lookup

--- midterm1datastructures/AList.java
+++ midterm1datastructures/AList.java
@@ -21,15 +21,25 @@ public class AList<T1> extends Object {
     }
 
     public void addFirst(T1 item) {
-
+        if (this.size() == this.items.length) {
+            this.resize(this.size() * 2);
+        }
+        this.items[this.nextFirst] = item;
+        this.nextFirst = Math.floorMod(this.nextFirst - 1, this.items.length);
+        this.size += 1;
     }
 
     public void addLast(T1 item) {
-
+        if (this.size() == this.items.length) {
+            this.resize(this.size() * 2);
+        }
+        this.items[this.nextLast] = item;
+        this.nextLast = Math.floorMod(this.nextLast + 1, this.items.length);
+        this.size += 1;
     }
 
     public int size() {
-        return -1;
+        return this.size;
     }
 
     public void printAList() {
@@ -37,7 +47,7 @@ public class AList<T1> extends Object {
     }
 
     public T1 removeFirst() {
-        return null;
+        this
     }
 
     public T1 removeLast() {
