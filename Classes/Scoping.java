274e213278855519bd21b96aee1ae3bc1932bdd3
Monishwaran <monishwaran@macbook.local.com>
2020-07-06 19:31:47 -0700

    Scoping Questions re-edit

--- Classes/Scoping.java
+++ Classes/Scoping.java
@@ -4,12 +4,13 @@ public class Scoping {
 		int y = 10;
 		class K {
 			int z = 7;
-
 			public K(){
 				this.z = x;
 			}
 			void printer(){
-				System.out.println(x); // Very Dangerous.
+				x = 10; // field attribute changing. 
+				System.out.println(x); // Very Dangerous. Ask Matt. Is java, like python, Lexically scoped? 
+				//System.out.println(this.x); // Is this implicit on inner method/field scope?
 			}
 		}
 
@@ -20,5 +21,6 @@ public class Scoping {
 	public static void main(String[] args) {
 		Scoping scop = new Scoping();
 		scop.method1();
+		System.out.println(scop.x);
 	}
 }
