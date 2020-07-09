6d62332bc6ca8b4137c8437df51998e96a848a4b
Monishwaran <monishwaran@macbook.local.com>
2020-07-09 09:52:19 -0700

    basic runtime.

--- Runtime/basicRuntime.java
+++ Runtime/basicRuntime.java
@@ -1,3 +1,18 @@
 public class basicRuntime {
-    
+
+    public static int problem1(int x){
+        int f = 0;
+        if(x == 0)
+            return 10;
+        for(int i = 0; i < x; i++){
+            f += problem1(x/2) + problem1(x - 1);
+            x = x/2;
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        problem1();
+    }
+
 }
