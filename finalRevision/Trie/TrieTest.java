19e5c128c3d825678f752fa17dc4fee56eb055f5
Monishwaran <monishwaran@macbook.local.com>
2020-08-11 13:40:00 -0700

    added trie

---
+++ finalRevision/Trie/TrieTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrieTest {
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void contains() {
+
+    }
+}
